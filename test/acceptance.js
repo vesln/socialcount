04688721402600a53a1c830430fe34dda64bd327
Veselin Todorov <veselin@veselin.bg>
2014-01-09 11:36:16 +0200

    Add test for tweet count == 0

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,10 +1,19 @@
 var social = require('../');
 var url = 'http://www.boredpanda.com/miniam-food-dioramas-pierre-javelle-akiko-ida/';
+var emptyUrl = 'http://example.com/social-count';
 
 test('get tweet count for url', function(done) {
   social(url, { tweets: true }, function(err, res) {
     assert(!err, 'unexpected error');
     assert(res.tweets > 0, 'zero tweet count');
+    done();
+  });
+});
+
+test('get tweet count for url with no tweets', function(done) {
+  social(emptyUrl, { tweets: true }, function(err, res) {
+    assert(!err, 'unexpected error');
+    assert(res.tweets === 0, 'non-zero tweet count');
     done();
   });
 });
