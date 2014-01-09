25cb43c753e4115169bdb015c9ea0372265693da
Veselin Todorov <veselin@veselin.bg>
2014-01-09 11:34:42 +0200

    Get tweet count for given url

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 /**
+ * External dependencies.
+ */
+
+var request = require('request');
+
+/**
+ * URLS.
+ */
+
+var URLS = {
+  tweets: 'http://urls.api.twitter.com/1/urls/count.json?url={url}'
+};
+
+/**
+ * Get the tweet count for `url`.
+ *
+ * @param {String} url
+ * @param {Function} callback
+ * @cb {Error} error
+ * @cb {Number} count
+ * @api private
+ */
+
+function tweets(url, fn) {
+  request.get(URLS.tweets.replace('{url}', url), { json: true }, function(err, res) {
+    if (err) return fn(err);
+    fn(null, res.body.count);
+  });
+}
+
+/**
  * Get the social count for `url`.
  *
  * Options:
@@ -14,7 +45,10 @@
  */
 
 function social(url, opts, fn) {
-  fn(null, { tweets: 3 });
+  tweets(url, function(err, count) {
+    if (err) return fn(err);
+    fn(null, { tweets: count });
+  });
 }
 
 /**
--- test/acceptance.js
+++ test/acceptance.js
@@ -1,9 +1,10 @@
 var social = require('../');
+var url = 'http://www.boredpanda.com/miniam-food-dioramas-pierre-javelle-akiko-ida/';
 
 test('get tweet count for url', function(done) {
-  social('http://google.com', { tweets: true }, function(err, res) {
-    assert(!err);
-    assert(res.tweets);
+  social(url, { tweets: true }, function(err, res) {
+    assert(!err, 'unexpected error');
+    assert(res.tweets > 0, 'zero tweet count');
     done();
   });
 });
